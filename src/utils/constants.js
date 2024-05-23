9882116e20c76222c64eef1e6efb4cfd4a7155f9
shusovan007 <86873977+shusovan007@users.noreply.github.com>
2024-05-24 00:56:26 +0530

    Update constants.js
    removed api key

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY="AIzaSyAKIfGMVCSYh_f5811EZmceywys7zzXuSM";
+const GOOGLE_API_KEY="xxxxxxxxxx";
 
 export const YOUTUBE_VIDEOS_API="https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=30&regionCode=IN&key="+GOOGLE_API_KEY;
 export const YOUTUBE_SEARCH_API="http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
